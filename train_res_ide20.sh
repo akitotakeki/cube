74d0dafa94bef6706456fced6db6d5bd7d358adf
takeki <takeki@nae-lab.org>
2016-06-22 22:38:32 +0900

    revise ide16,20

--- train_res_ide20.sh
+++ train_res_ide20.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-/home/takeki/caffe/build/tools/caffe train --solver=res_ide20_solver.prototxt --gpu=1
+/home/takeki/caffe/build/tools/caffe train --solver=res_ide20_solver.prototxt --gpu=0
